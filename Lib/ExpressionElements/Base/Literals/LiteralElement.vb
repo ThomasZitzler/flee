b4568f3b89e88377a3e98f3ddac379adde8224ae
zitzltho <thomas.zitzler@tieto.com>
2015-11-24 11:49:29 +0100

    Proper .gitignore file, upgrading project to VS2105

--- Lib/ExpressionElements/Base/Literals/LiteralElement.vb
+++ Lib/ExpressionElements/Base/Literals/LiteralElement.vb
@@ -2,17 +2,17 @@
 ' modify it under the terms of the GNU Lesser General Public License
 ' as published by the Free Software Foundation; either version 2.1
 ' of the License, or (at your option) any later version.
-' 
+'
 ' This library is distributed in the hope that it will be useful,
 ' but WITHOUT ANY WARRANTY; without even the implied warranty of
 ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ' Lesser General Public License for more details.
-' 
+'
 ' You should have received a copy of the GNU Lesser General Public
 ' License along with this library; if not, write to the Free
 ' Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 ' MA 02111-1307, USA.
-' 
+'
 ' Flee - Fast Lightweight Expression Evaluator
 ' Copyright © 2007 Eugene Ciloci
 '
@@ -84,6 +84,7 @@ Friend MustInherit Class LiteralElement
 				ldcOpcode = OpCodes.Ldc_I4_M1
 			Case Else
 				Debug.Assert(False, "value out of range")
+                ldcOpcode = OpCodes.Nop
 		End Select
 
 		ilg.Emit(ldcOpcode)
